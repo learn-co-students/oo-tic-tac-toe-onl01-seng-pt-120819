4a1cd9432514a988daa9cab3311887ef4b7f3d7c
Amanda Rolon <abrolon87@gmail.com>
2019-12-31 23:31:37 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,6 @@
 class TicTacToe
   def initialize
-    @board = Array.new(9, " ")
+    @board = Array.new(9, " ")#[" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
 
   WIN_COMBINATIONS = [
@@ -22,12 +22,71 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-  def input_to_index(string)
-    gets = string.to_i-1
+  def input_to_index(user_input)
+    user_input.to_i-1
   end
 
-  def move
-
+  def move(i, t = "X")
+    @board[i] = t
   end
 
+  def position_taken?(i)
+    @board[i] != " "
+  end
+
+  def valid_move?(i)
+    !position_taken?(i) && i.between?(0,8)
+  end
+
+  def turn
+    puts "Enter a number(1-9):"
+    user_input = gets.strip
+    i = input_to_index(user_input)
+    if valid_move?(i)
+      t = current_player
+      move(i, t)
+    else
+      turn
+    end
+    display_board
+  end
+
+  def turn_count
+    @board.count{|spot| spot != " " }
+  end
+
+  def current_player
+    turn_count.even? ? "X" : "O"
+  end
+
+  def won?
+    WIN_COMBINATIONS.any? do |combo|
+      if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
+        return combo
+      end
+    end
+  end
+
+  def full?
+    @board.all?{|spot| spot != " " }
+  end
+
+  def draw?
+    full? && !won?
+  end
+
+  def over?
+    won? || draw?
+  end
+
+  def winner
+    if combo = won?
+      @board[combo[0]]
+    end
+  end
+
+  def play
+    turn until over?
+    puts winner ? "Congratulations #{winner}!" : "Cat's Game!"
+  end
 end
