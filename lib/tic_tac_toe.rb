cbb0b1b8255d9e398b5089f6c05fd1a2b058793d
Catherine Smiley <catherinesmiley@gmail.com>
2020-01-12 16:49:11 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,19 +27,19 @@ class TicTacToe
   end
   
   def input_to_index(user_input)
-    input = user_input.to_i-1
+    user_input.to_i-1
   end
   
   def move(index, token = "X")
     @board[index] = token
   end
   
-  def position_taken?(index_i)
-    @board[index_i] == "X" || @board[index_i] == "O"
+  def position_taken?(index)
+    ((@board[index] == "X") || (@board[index] == "O"))
   end
     
-    def valid_move?(index_i)
-      @board.include?(@board[index_i]) && @board[index_i] == " "
+    def valid_move?(index)
+      index.between?(0,8) && !position_taken?(index)
     end
     
     def turn_count
@@ -61,41 +61,66 @@ class TicTacToe
     end
     
     def turn 
-      current_player
-      puts "Please specify a position between 1-9"
+      puts "Please choose a number 1-9:"
       input = gets.chomp 
       index = input_to_index(input)
+      player_token = current_player
       if valid_move?(index)
-        move(index)
+        move(index, player_token) 
         display_board
       else 
         turn
       end
     end
+    
+    def won?
+      WIN_COMBINATIONS.detect do |array|
+        if @board[array[0]] == "X" && @board[array[1]] == "X" && @board[array[2]] == "X"
+          return array 
+        elsif 
+        @board[array[0]] == "O" && @board[array[1]] == "O" && @board[array[2]] == "O"
+          return array
+        end
+          false
+      end
+    end 
+    
+    def full?
+      @board.all? {|element| element == "X" || element == "O"}
+    end
+    
+    def draw?
+      !won? && full?
+    end
+    
+    def over?
+      !!won? || draw?
+    end
+    
+    def winner
+      WIN_COMBINATIONS.detect do |array|
+        if @board[array[0]] == "X" && @board[array[1]] == "X" && @board[array[2]] == "X"
+          return "X" 
+        elsif 
+        @board[array[0]] == "O" && @board[array[1]] == "O" && @board[array[2]] == "O"
+          return "O"
+        else
+          nil
+        end
+      end
+    end
+    
+    def play
+      until over? == true
+        turn
+      end
+    
+      if won?
+        puts "Congratulations #{winner}!"
+      elsif draw?
+        puts "Cat's Game!"
+      end
+    end
 
+    
 end
-
-
-
-
-
-# def won?
-#       WIN_COMBINATIONS.each do |array|
-#         if @board.include?(array)
-#           array 
-#         else
-#           false
-#         end
-#         binding.pry
-#       end
-#     end 
-
-
-
-
-
-
-
-
-
-
